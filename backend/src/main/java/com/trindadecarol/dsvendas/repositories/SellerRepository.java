58bc8cf8e107603a3966ecddfd9623c59ebf1725
Carol Trindade <caroltrindade6@gmail.com>
2021-09-12 11:19:26 -0300

    Layers

---
+++ backend/src/main/java/com/trindadecarol/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.trindadecarol.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trindadecarol.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/trindadecarol/dsvendas/services/SellerService.java
@@ -0,0 +1,30 @@
+package com.trindadecarol.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trindadecarol.dsvendas.dto.SellerDTO;
+import com.trindadecarol.dsvendas.entities.Seller;
+import com.trindadecarol.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+@Autowired
+ private SellerRepository repository;	
+	
+
+ public List<SellerDTO> findAll(){
+	 List<Seller> result = repository.findAll();
+	 
+	 
+	 return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+ }
+ 
+ 
+ 
+
+}
